7597164a2f35f01263ca52c09fac77f18c6c31fb
wendellvsilva <wendellcrtss@gmail.com>
2024-01-23 20:31:24 -0300

    Tiposdetreino
    adicionei os 3 tipos de treino necessários

--- src/controle/Main.java
+++ src/controle/Main.java
@@ -1,103 +1,42 @@
 package controle;
 
-import java.util.Date;  
 import java.util.Scanner;
-
-import entidades.FichaTreino;
-import entidades.Professor;
-import entidades.Cliente;
+import entidades.TreinoCostas;
+import entidades.TreinoPeito;
+import entidades.TreinoPerna;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner resposta = new Scanner(System.in);
-        int resp1 = 0;
+        try (Scanner resposta = new Scanner(System.in)) {
+            int resp1;
 
-        System.out.println("Olá! Bem-vindo ao DevTraining!");
-        System.out.println("Digite '1' se você é aluno ou '2' se você é professor.");
-        resp1 = resposta.nextInt();
+            System.out.println("Olá! Bem-vindo ao DevTraining!");
+            System.out.println("Digite '1' se você é aluno ou '2' se você é professor.");
+            resp1 = resposta.nextInt();
 
-        if (resp1 == 1) {
-            System.out.println("Olá aluno!\n");
-            System.out.println("Digite o tipo de treino que você deseja:");
-            System.out.println("1 - Inferior");
-            System.out.println("2 - Costas");
-            System.out.println("3 - Peito");
-            int TipoTreinoResposta1 = 0;
-            Scanner TipoTreinoResposta = new Scanner(System.in);
-            TipoTreinoResposta1 = TipoTreinoResposta.nextInt();
+            if (resp1 == 1) {
+                System.out.println("Olá aluno!");
+                System.out.println("Digite o tipo de treino que você deseja:");
+                System.out.println("1 - Inferior");
+                System.out.println("2 - Costas");
+                System.out.println("3 - Peito");
 
-            if(TipoTreinoResposta1 == 1) {
-                Professor professor1 = new Professor("Wendell", "874243252", "wendell.stronda@email.com", "515141414", 21, 78, "manhã", 98);
-                Cliente cliente = new Cliente("Cliente Teste", "423423456", "cliente@email.com", "898444111-89", 97, "Rua robertinho", "treino", null, professor1, false);
-                FichaTreino ficha = new FichaTreino(1, professor1, cliente, new Date(), "Inferior");
+                int tipoTreinoResposta = resposta.nextInt();
 
-                ficha.adicionaExercicio("Agachamento 3x8\n");
-                ficha.adicionaExercicio("Extensora 3x8\n");
-                ficha.adicionaExercicio("Hack Articulado 3x8\n");
-                ficha.adicionaExercicio("Mesa Flexora 3x8\n");
-
-                System.out.println("Detalhes do Treino:");
-                System.out.println("ID do Treino: " + ficha.getIdTreino());
-                System.out.println("Professor: " + ficha.getProfessor().getNome());
-                System.out.println("Cliente: " + ficha.getCliente().getNome());
-                System.out.println("Data de Criação: " + ficha.getDataCriacao());
-                System.out.println("Tipo de Treino: " + ficha.getTipoTreino());
-                System.out.println("Exercícios:");
-                for (String exercicio : ficha.getExercicios()) {
-                    System.out.println(exercicio);
+                if (tipoTreinoResposta == 1) {
+                    TreinoPerna.executarTreino("Inferior");
+                } else if (tipoTreinoResposta == 2) {
+                    TreinoCostas.executarTreino("Costas");
+                } else if (tipoTreinoResposta == 3) {
+                    TreinoPeito.executarTreino("Peito");
+                } else {
+                    System.out.println("Opção inválida!");
                 }
-                System.out.println("BOM TREINO!");
-
-            } else if (TipoTreinoResposta1 == 2) {
-                Professor professor1 = new Professor("Wendell", "874243252", "wendell.stronda@email.com", "515141414", 21, 78, "manhã", 98);
-                Cliente cliente = new Cliente("Cliente Teste", "423423456", "cliente@email.com", "898444111-89", 97, "Rua robertinho", "treino", null, professor1, false);
-                FichaTreino ficha = new FichaTreino(1, professor1, cliente, new Date(), "Costas");
-
-                ficha.adicionaExercicio("Remada Unilateral com Halteres 3x8\n");
-                ficha.adicionaExercicio("Puxada alta na polia 3x8\n");
-                ficha.adicionaExercicio("Remada com barra 3x8\n");
-                ficha.adicionaExercicio("Barra fixa 3x8\n");
-
-                System.out.println("Detalhes do Treino:");
-                System.out.println("ID do Treino: " + ficha.getIdTreino());
-                System.out.println("Professor: " + ficha.getProfessor().getNome());
-                System.out.println("Cliente: " + ficha.getCliente().getNome());
-                System.out.println("Data de Criação: " + ficha.getDataCriacao());
-                System.out.println("Tipo de Treino: " + ficha.getTipoTreino());
-                System.out.println("Exercícios:");
-                for (String exercicio : ficha.getExercicios()) {
-                    System.out.println(exercicio);
-                }
-                System.out.println("BOM TREINO!");
-
-            } else if (TipoTreinoResposta1 == 3) {
-                Professor professor1 = new Professor("Wendell", "874243252", "wendell.stronda@email.com", "515141414", 21, 78, "manhã", 98);
-                Cliente cliente = new Cliente("Cliente Teste", "423423456", "cliente@email.com", "898444111-89", 97, "Rua robertinho", "treino", null, professor1, false);
-                FichaTreino ficha = new FichaTreino(1, professor1, cliente, new Date(), "Peito");
-
-                ficha.adicionaExercicio("Supino reto 3x8\n");
-                ficha.adicionaExercicio("Supino inclinado 3x8\n");
-                ficha.adicionaExercicio("Crossover 3x8\n");
-                ficha.adicionaExercicio("Voador 3x8\n");
-
-                System.out.println("Detalhes do Treino:");
-                System.out.println("ID do Treino: " + ficha.getIdTreino());
-                System.out.println("Professor: " + ficha.getProfessor().getNome());
-                System.out.println("Cliente: " + ficha.getCliente().getNome());
-                System.out.println("Data de Criação: " + ficha.getDataCriacao());
-                System.out.println("Tipo de Treino: " + ficha.getTipoTreino());
-                System.out.println("Exercícios:");
-                for (String exercicio : ficha.getExercicios()) {
-                    System.out.println(exercicio);
-                }
-                System.out.println("BOM TREINO!");
+            } else if (resp1 == 2) {
+                System.out.println("Olá professor!");
+            } else {
+                System.out.println("Opção inválida!");
             }
-
-        } else if (resp1 == 2) {
-            System.out.println("Olá professor!");
-        
-        } else {
-            System.out.println("Opção inválida!");
         }
     }
 }
--- src/entidades/FichaTreino.java
+++ src/entidades/FichaTreino.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class FichaTreino {
+
+public class FichaTreino  {
 
     private int idTreino;
     private Professor professor;
@@ -47,35 +48,15 @@ public class FichaTreino {
         if (indice >= 0 && indice < exercicios.size()) {
             this.exercicios.set(indice, novoExercicio);
         } else {
-            System.out.println("XXXXXXXX");//dar ideias pra oq colocar aqui
+            System.out.println("ERRO");
         }
     }
     public void removeExercicio(int indice) {
         if (indice >= 0 && indice < exercicios.size()) {
             this.exercicios.remove(indice);
         } else {
-            System.out.println("XXXXXXXXXXXXX");//dar ideias pra oq colocar aqui
+            System.out.println("ERRO");//dar ideias pra oq colocar aqui
         }
     }
-
-    public static void main(String[] args) {
-    	Professor professor1 = new Professor("Wendell Stronda", "874243252", "wendell.stronda@email.com", "515141414", 21, 78, "manhã", 98);
-        Cliente cliente = new Cliente("Cliente Teste", "423423456", "cliente@email.com", "898444111-89", 97, "Rua robertinho", "treino", null, professor1, false);
-        FichaTreino ficha = new FichaTreino(1, professor1, cliente, new Date(), "Perna");
-        ficha.adicionaExercicio("Agachamento");//aqui a gente adiciona
-                                              // os exercicios na ficha de treino
-        ficha.adicionaExercicio("EXTENSORA BRABISSIMA");//.
-
-        System.out.println("ID do Treino: " + ficha.getIdTreino());
-        System.out.println("Professor: " + ficha.getProfessor().getNome());
-        System.out.println("Cliente: " + ficha.getCliente().getNome());
-        System.out.println("Data de Criação: " + ficha.getDataCriacao());
-        System.out.println("Tipo de Treino: " + ficha.getTipoTreino());
-        System.out.println("Exercícios: " + ficha.getExercicios());
-        ficha.alteraExercicio(0, "Agachamento Livre");//altera o exercicio na ficha
-        ficha.removeExercicio(1);//remove o exercicio
-
-        System.out.println("Exercícios dps da alteração ou remoção: "
-                + ficha.getExercicios());  //a gente vai atualizando isso no decorrer do código..
-    }
+    
 }
