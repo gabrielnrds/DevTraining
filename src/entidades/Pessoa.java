6f062babaee74dbfa8712cdf9d82f0e6c6b9f267
wendellvsilva <wendellcrtss@gmail.com>
2024-01-25 19:24:20 -0300

    hashcode
    hashcode

--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -1,6 +1,6 @@
 package entidades;
 
-public class Pessoa {
+public  abstract class Pessoa {
     private String nome;
     private String telefone;
     private String email;
@@ -17,6 +17,52 @@ public class Pessoa {
 
     public String getNome() {
         return nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + idade;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (idade != other.idade)
+            return false;
+        return true;
     }
 
     public void setNome(String nome) {
